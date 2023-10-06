f1c1acef762fc9a3ffd1eefee071882f5f8d188c
Kajitech <124563715+Kajitech@users.noreply.github.com>
2023-10-07 06:00:34 +0900

    Add files via upload

---
+++ bluetooth_address_check/bluetooth_address_check.ino
@@ -0,0 +1,14 @@
+//#include "M5Atom.h"
+
+uint8_t btmac[6];
+
+void setup() {
+  Serial.begin(115200);
+  esp_read_mac(btmac, ESP_MAC_BT);
+  Serial.printf("[Bluetooth] Mac Address = %02X:%02X:%02X:%02X:%02X:%02X\r\n", btmac[0], btmac[1], btmac[2], btmac[3], btmac[4], btmac[5]);
+}
+
+void loop() {
+  Serial.printf("[Bluetooth] Mac Address = %02X:%02X:%02X:%02X:%02X:%02X\r\n", btmac[0], btmac[1], btmac[2], btmac[3], btmac[4], btmac[5]);
+  delay(1000);
+}
